527062c9b69e226167c712421949f406a089988e
影舞者 <39651966+shadow1ng@users.noreply.github.com>
2020-07-09 15:11:53 +0800

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -3,132 +3,185 @@ import nmap
 import argparse
 import datetime
 import threading
-import requests
 import re
-import sys
 import os
 import time
 import subprocess
 from IPy import IP
 import re
-requests.packages.urllib3.disable_warnings()
 from queue import Queue
-limitNumber = 50
+from urllib.parse import urlparse
+import socket
+import platform
+import requests
+requests.packages.urllib3.disable_warnings()
+limitNumber = 100
 lock = threading.Lock()
-final_domains = []
-final_port={}
+webqueue = Queue()
+LOGFILE="result.txt"
 
 
 class PortScan(threading.Thread):
-    def __init__(self, queue,level,porsfw):
+    def __init__(self, queue,level,ports):
         threading.Thread.__init__(self)
         self._queue = queue
         self.level=level
-        self.porsfw=porsfw
+        self.ports=ports
 
     def run(self):
-
         while not self._queue.empty():
             scan_ip = self._queue.get()
+            time.sleep(0.01)
             try:
-                ports = portscan(scan_ip,self.porsfw)
-                time.sleep(1)
-                if int(self.level) >0:
-                    if len(ports)>0:
-                        Scan(scan_ip, ports)
-                        print_result(scan_ip)
+                domain=None
+                if(bool(re.search('[a-zA-Z]', scan_ip))):
+                    if "://" in scan_ip:
+                        domain=urlparse(scan_ip).netloc
+                    else:
+                        domain=scan_ip
+                    scan_ip=(socket.gethostbyname(domain))
+            except Exception as e:
+                continue
 
-                else:
-                    ports_result(scan_ip,ports)
+            try:
+                openports = self.MassScan(scan_ip,self.ports)
+                log("port",scan_ip,openports)
+                if len(openports)>0:
+                    service = self.NmapScan(scan_ip, openports)
+                    if domain:
+                        log("domain",scan_ip,service,domain=domain)
+                    else:
+                        log("service",scan_ip,service)
+
             except Exception as e:
                 print(e)
                 pass
-def ports_result(scan_ip,ports):
-        f1 = open('./result.txt', 'a', encoding='utf-8')
-        for port in ports:
-            line={"port":scan_ip+":"+str(port),"service":""}
-            f1.write(str(line)+"\n")
-        f1.close()
-def print_result(scan_ip):
-    for i in final_port:
-        if i ==scan_ip:
-            f1 = open('./result.txt', 'a', encoding='utf-8')
-            for port in final_port[i]:
-                line={"port":scan_ip+":"+str(port),"service":final_port[i][port]}
-                f1.write(str(line)+"\n")
-            f1.close()
 
-#调用masscan
-def portscan(scan_ip,porsfw):
-    try:
-        tmp_ports=[]
-        ports = []
-        command = './masscan {} -p {} --rate 1000 --wait 1'.format(scan_ip,porsfw)
-        child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-        while child.poll() is None:
-            output = child.stdout.readline()
-            line = str(output, encoding='utf-8').strip()
-            if "Discovered" in line:
-
-                if re.findall(r'port (\d{1,5})/tcp',line):
-                    port = re.findall(r'port (\d{1,5})/tcp',line)[0]
-                    tmp_ports.append(port)
-                foundNumber = len(tmp_ports)
-                if int(foundNumber) > int(limitNumber):
-                    os.kill(child.pid, 9)
-                    print("有waf")
-        if len(tmp_ports)>limitNumber:
-            tmp_ports.clear()
-        else:
-            ports.extend(tmp_ports)
-        print(scan_ip+" ports "+str(ports))
-        return ports
-    except Exception as e:
-        print(e)
-
-#调用nmap识别服务
-def Scan(scan_ip,ports):
-    service ={}
-    nm = nmap.PortScanner()
-    ports=(','.join(ports))
-    try:
-        start_time = datetime.datetime.now()
-        ret = nm.scan(scan_ip,ports,arguments='-sS -Pn --version-all')#--open --version-all --host-timeout 20
-        tcp=(ret['scan'][scan_ip]['tcp'])
-        for port in tcp:
-            if tcp[port]['name']:
-                service[port]=tcp[port]['name'].replace("sun-answerbook", "http")
+    #run_masscan
+    def MassScan(self,scan_ip,porsfw):
+        try:
+            tmp_ports=[]
+            ports = []
+            sys = platform.system()
+            if sys == "Windows":
+                command = 'masscan.exe {} -p {} --rate 1000 --wait 1'.format(scan_ip,porsfw)
             else:
-                service[port]="unknown"
-    except Exception as e:
-       print(e)
-       pass
-    finally:
-        print(scan_ip+" service "+str(service))
-        final_port[scan_ip] = service
+                command = './masscan {} -p {} --rate 1000 --wait 1'.format(scan_ip,porsfw)
+            print(command)
+            child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            while child.poll() is None:
+                output = child.stdout.readline()
+                line = str(output, encoding='utf-8').strip()
+                # print(line)
+                if "Discovered" in line:
+                    print(line)
+                    if re.findall(r'port (\d{1,5})/tcp',line):
+                        port = re.findall(r'port (\d{1,5})/tcp',line)[0]
+                        tmp_ports.append(port)
+                    foundNumber = len(tmp_ports)
+                    if int(foundNumber) > int(limitNumber):
+                        os.kill(child.pid, 9)
+                        print("有waf")
+            if len(tmp_ports)>limitNumber:
+                tmp_ports.clear()
+            else:
+                ports.extend(tmp_ports)
+            return ports
+        except Exception as e:
+            print(e)
 
-#启用多线程扫描
-def main(target,level,ports,nums):
-    queue = Queue()
-    try:
-        ip_list=get_ip_list(target)
-        for final_ip in ip_list:
-            queue.put(final_ip)
-        threads = []
-        thread_count = nums
-        for i in range(thread_count):
-            threads.append(PortScan(queue,level,ports))
+    #run_nmap
+    def NmapScan(self,scan_ip,ports):
+        service ={}
+        nm = nmap.PortScanner()
+        ports=(','.join(ports))
+        try:
+            ret = nm.scan(scan_ip,ports,arguments='-sS -Pn --version-all')#--open --version-all --host-timeout 20
+            tcp=(ret['scan'][scan_ip]['tcp'])
+            for port in tcp:
+                if tcp[port]['name']:
+                    service[port]=tcp[port]['name'].replace("sun-answerbook", "http")
+                else:
+                    service[port]="unknown"
+            return service
+        except Exception as e:
+            print(e)
+            pass
 
-        for t in threads:
-            t.start()
+class HttpScan(threading.Thread):
+    def __init__(self,queue):
+        threading.Thread.__init__(self)
+        self._queue = queue
 
-        for t in threads:
-            t.join()
+    def gettile(self,ip,port, proxy=None, timeout=10):
+        try:
+            if int(port) == 443:
+                url = "https://{}:{}".format(ip, port)
+            else:
+                url = "http://{}:{}".format(ip, port)
+            headers = {
+                'Connection': 'close',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate, sdch, br',
+                'Accept-Language': 'zh-CN,zh;q=0.8',
+            }
+                      
+            if proxy:
+                proxy = {'http': proxy,'https': proxy}
+            # proxy = {'http': '127.0.0.1:8080','https': '127.0.0.1:8080'}
+
+            req  = requests.get(url, headers=headers,proxies=proxy, timeout=timeout,verify=False)
+            
+            status = req.status_code
+            if status == 400:
+                url = "https://{}:{}".format(ip, port)
+                req  = requests.get(url, headers=headers,proxies=proxy, timeout=timeout,verify=False)
+                status = req.status_code
+            
+            code = re.search(r'<meta.*charset="?([\w|-]*)"?\s*/?>', req.text,re.IGNORECASE)
+            if code:
+                code = code.group(1)
+            else:
+                code = "utf-8"
+            req.encoding=code
+            
+            title = re.search(r'<title>(.*)</title>', req.text,re.S)
+            if title:
+                title = title.group(1).strip().strip("\r").strip("\n")[:30]
+            else:
+                title = "None"
+            banner = ''
+            if 'Server' in req.headers:
+                banner = req.headers['Server'][:20]
+            log=("%-30s| %-6s| %-20s| %-30s" % (url, status, banner, title))
+            lock.acquire()
+            print(log)
+            print("+---------------------------------------------------------------------------+")
+            log_file = open(LOGFILE,'a',encoding='utf-8')
+            log_file.write(log+"\n")
+            log_file.close()
+            lock.release()
+        except Exception as e:
+            # log=("%-30s| %-20s" % (url, e))
+            # print(log)
+            pass
 
 
-    except Exception as e:
-        print(e)
-        pass
+
+    def run(self):
+        time.sleep(0.01)
+        while not self._queue.empty():
+            try:
+                queue_task = self._queue.get(timeout=0.5)
+                task_host,task_port = queue_task.split(":")
+                self.gettile(task_host,task_port)
+            except:
+                pass
+
+
+
+
 
 def get_ip_list(ipin):
     ipdo=[]
@@ -167,21 +220,92 @@ def get_ip_list(ipin):
                     ip_list.append(str(x))
             else:
                 ip_list.append(ipone)
-    return ip_list
+    return list(set(ip_list))
+
+
+def log(log_type,scan_ip,info,domain=None):
+    lock.acquire()
+    if log_type == "port":
+        for port in info:
+            output = "{}:{} open".format(scan_ip,port)
+            webqueue.put(":".join([scan_ip,port]))
+            print(output)
+    elif log_type == "service":
+        for port in info:
+            output = '{}:{} is {}'.format(scan_ip,port,info[port])
+            print(output)
+            f = open(LOGFILE, 'a+', encoding='utf-8')
+            f.write(str(output)+"\n")
+    elif log_type == "domain":
+        for port in info:
+            output = '{}:{} {} is {}'.format(scan_ip,port,domain,info[port])
+            webqueue.put(":".join([domain,str(port)]))
+            print(output)
+            f = open(LOGFILE, 'a+', encoding='utf-8')
+            f.write(str(output)+"\n")
+    lock.release()
+
+
+
+
+
+
+def run_port(target,level,ports,nums):
+    queue = Queue()
+    threads = []
+    try:
+        ip_list = get_ip_list(target)
+        for ip in ip_list:
+            queue.put(ip)
+
+        for i in range(nums):
+            threads.append(PortScan(queue,level,ports))
+
+        for t in threads:
+            t.start()
+
+        for t in threads:
+            t.join()
+
+    except Exception as e:
+        print(e)
+        pass
+
+
+
+def run_http(webqueue,threadNum =100):
+    threads = []
+    for num in range(1,threadNum + 1):
+        threads.append(HttpScan(webqueue))
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+#启用多线程扫描
+def main(target,level,ports,nums):
+    run_port(target,level,ports,nums)
+    if level>0:
+        run_http(webqueue)
+
+
 
 if __name__ =='__main__':
     start_time = datetime.datetime.now()
     parser = argparse.ArgumentParser(usage='python3 dirscan.py --target [source urls file]')
     parser.add_argument("-t","--target", type=str, help="192.168.1.0/24 or ip or ips or file.")
     parser.add_argument("-p","--ports", type=str, default="1-65535")
-    parser.add_argument("-n","--nums", type=int, default=5)
-    parser.add_argument("-v","--level", type=int, help="-v  skip nmap",default="1")
+    parser.add_argument("-n","--nums", type=int, default=10)
+    parser.add_argument("-v","--level", type=int, help="-v 0 not run webtitle",default="1")
+    parser.add_argument("-o","--output", type=str, help="outfile",default="result.txt")
     args = parser.parse_args()
-    urls_file = args.target
+    target = args.target
     ports=args.ports
     level=args.level
     nums=args.nums
-
-    main(urls_file,level,ports,nums)
+    LOGFILE=args.output
+    main(target,level,ports,nums)
     spend_time = (datetime.datetime.now() - start_time).seconds
     print('程序共运行了： ' + str(spend_time) + '秒')
